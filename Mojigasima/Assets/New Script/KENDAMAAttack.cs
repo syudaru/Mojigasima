12dbb0a73b9e5098221b7e3aa10764460a3921d2
syudaru <syudaru@gmail.com>
2019-11-08 18:03:12 +0900

    KENDAMA

--- Mojigasima/Assets/New Script/KENDAMAAttack.cs
+++ Mojigasima/Assets/New Script/KENDAMAAttack.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class KENDAMAAttack : MonoBehaviour
 {
+    public GameObject KENDAMA = null;
+    public Vector3 pos = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class KENDAMAAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = KENDAMA.transform.position + pos;
+
     }
 }
