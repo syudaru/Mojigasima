fe963efdebf5ccb1652199b7cba22eb26be28344
hiroshiamako <57023471+hiroshiamako@users.noreply.github.com>
2019-12-02 22:04:06 +0900

    a

---
+++ Mojigasima/Assets/New Script/BossKu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossKu : MonoBehaviour
+{
+    public float speed = 10;
+    public float angle = 90;
+
+     void Update()
+    {
+        this.transform.Translate(speed / 50, 0, 0);
+        this.transform.Rotate(0, 0, angle / 50);
+    }
+}
--- Mojigasima/Assets/New Script/PrehabEnemy.cs
+++ Mojigasima/Assets/New Script/PrehabEnemy.cs
@@ -9,35 +9,36 @@ public class PrehabEnemy : MonoBehaviour
     public GameObject bulletPrefab; // 弾のプレハブ
     public float time; // 経過時間(秒)
     public float speed = 2;
+    private bool isInsideCamera;
+
     // 毎フレーム呼び出されるメソッド
 
+
     void Update()
+
     {
-        this.transform.Translate(speed / 50, 0, 0);
-    time += Time.deltaTime;
-        // １秒ごとに弾を発射
-        if (time > 2.0f)
-        { // 経過時間が１秒より大きければ経過時間をリセット
-            time -= 2.0f;
-            GameObject obj;
-            obj = Instantiate(bulletPrefab);
-            obj.transform.position = transform.position;
-            obj.name = "EnemyBullet";
+        if (isInsideCamera)
+        {
+            this.transform.Translate(speed / 50, 0, 0);
+            time += Time.deltaTime;
+            // １秒ごとに弾を発射
+            if (time > 2.0f)
+            { // 経過時間が１秒より大きければ経過時間をリセット
+                time -= 2.0f;
+                GameObject obj;
+                obj = Instantiate(bulletPrefab);
+                obj.transform.position = transform.position;
+                obj.name = "EnemyBullet";
+            }
         }
     }
     void OnBecameInvisible()
-
     {
-
-        enabled = false;
-
+        Destroy(this.gameObject);
     }
 
-    void OnBecameVisible()
-
+    private void OnBecameVisible()
     {
-
-        enabled = true;
-
+        isInsideCamera = true;
     }
 }
