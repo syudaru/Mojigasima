2d712165578668c1252b91a2c319776d55d566fe
kouki <kouki072817@icloud.com>
2019-12-06 13:48:53 +0900

    ぐ、の攻撃方法

---
+++ Mojigasima/Assets/Yonemoto/script/GuAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuAttack : MonoBehaviour
+{
+	public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		GameObject target = GameObject.FindWithTag("Player");
+		float following = Time.deltaTime * speed;
+		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, following);
+		Destroy(this.gameObject, 3.0f);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
