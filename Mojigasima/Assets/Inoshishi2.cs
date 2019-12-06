4cf8db79e26df2d98168b6ff0371e576f236130d
hiroshiamako <57023471+hiroshiamako@users.noreply.github.com>
2019-12-06 13:40:30 +0900

    boss

---
+++ Mojigasima/Assets/Inoshishi2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inoshishi2 : MonoBehaviour
+{
+    public float speed = 1;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.Translate(speed / 100, 0, 0);
+    }
+}
