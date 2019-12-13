d400e696097a6cbf647fd5abcbc74d837e2f5d71
hiroshiamako <57023471+hiroshiamako@users.noreply.github.com>
2019-12-13 11:16:01 +0900

    ma desu

---
+++ Mojigasima/Assets/Origin/Enemy Script/Shake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public float limitSec = 3;
+
+    void Start()
+    {
+        Destroy(this.gameObject, limitSec);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Mojigasima/Assets/Origin/Enemy Script/Sometime_RandomCreatePrehab.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sometime_RandomCreatePrehab : MonoBehaviour
+{
+    public GameObject newPrehab;
+    public float intervalSec = 1;
+
+    void Start()
+    {
+        InvokeRepeating("CreatePrehab", intervalSec, intervalSec);
+    }
+
+    // Update is called once per frame
+    void CreatePrehab()
+    {
+        Vector3 area = GetComponent<SpriteRenderer>().bounds.size;
+        Vector3 newPos = this.transform.position;
+        newPos.x += Random.Range(-area.x / 2, area.x / 2);
+        newPos.y += Random.Range(-area.y / 2, area.y / 2);
+
+        GameObject newGameObject = Instantiate(newPrehab) as GameObject;
+        newGameObject.transform.position = newPos;
+    }
+}
